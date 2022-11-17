802cb146802a7bf836060ddc908871241b36e700
gallardo0825 <hirokix825@gmail.com>
2022-11-17 00:24:51 -1000

    プロジェクト ファイルを追加します。

---
+++ Logintest/Data/Migrations/20221108043952_CustomizeUserTable.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Logintest.Data.Migrations
+{
+    public partial class CustomizeUserTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Logintest/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Logintest.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
